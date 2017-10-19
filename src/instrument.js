a940c27e71773884ede26cf1bdd83850eadd6ee4
Peter Marton <peter@risingstack.com>
2017-10-19 11:21:15 +0200

    chore(package): update dependencies

--- src/instrument.js
+++ src/instrument.js
@@ -44,9 +44,8 @@ class Instrument {
     const instrumentedModules = _.uniq(instrumentations.map((instrumentation) => instrumentation.module))
 
     // Instrunent modules: hook require
-    hook(instrumentedModules, (moduleExports, moduleName, moduleBaseDir) => {
-      return this.hookModule(moduleExports, moduleName, moduleBaseDir)
-    })
+    hook(instrumentedModules, (moduleExports, moduleName, moduleBaseDir) =>
+      this.hookModule(moduleExports, moduleName, moduleBaseDir))
 
     debug('Patched')
   }
@@ -59,39 +58,39 @@ class Instrument {
   * @param {string} moduleName
   * @param {string} moduleBaseDir
   */
-  hookModule(moduleExports, moduleName, moduleBaseDir) {
-      let moduleVersion
+  hookModule (moduleExports, moduleName, moduleBaseDir) {
+    let moduleVersion
 
-      // Look for version in package.json
-      if (moduleBaseDir) {
-        const packageJSON = path.join(moduleBaseDir, 'package.json')
-        // eslint-disable-next-line
+    // Look for version in package.json
+    if (moduleBaseDir) {
+      const packageJSON = path.join(moduleBaseDir, 'package.json')
+      // eslint-disable-next-line
         moduleVersion = require(packageJSON).version
-      }
+    }
 
-      // Apply instrumentations
-      instrumentations
-        .filter((instrumentation) => instrumentation.module === moduleName)
-        .filter((instrumentation) => {
-          // Core modules don't have version number
-          if (_.isUndefined(moduleVersion) || !_.isArray(instrumentation.supportedVersions)) {
-            return true
-          }
+    // Apply instrumentations
+    instrumentations
+      .filter((instrumentation) => instrumentation.module === moduleName)
+      .filter((instrumentation) => {
+        // Core modules don't have version number
+        if (_.isUndefined(moduleVersion) || !_.isArray(instrumentation.supportedVersions)) {
+          return true
+        }
 
-          return instrumentation.supportedVersions.some((supportedVersion) =>
-            semver.satisfies(moduleVersion, supportedVersion))
+        return instrumentation.supportedVersions.some((supportedVersion) =>
+          semver.satisfies(moduleVersion, supportedVersion))
+      })
+      .forEach((instrumentation) => {
+        instrumentation.patch(moduleExports, this._tracers, this._options)
+        this._instrumented.set(moduleExports, instrumentation)
+
+        debug(`Instrumentation "${instrumentation.name}" applied on module "${moduleName}"`, {
+          moduleVersion,
+          supportedVersions: instrumentation.supportedVersions
         })
-        .forEach((instrumentation) => {
-          instrumentation.patch(moduleExports, this._tracers, this._options)
-          this._instrumented.set(moduleExports, instrumentation)
+      })
 
-          debug(`Instrumentation "${instrumentation.name}" applied on module "${moduleName}"`, {
-            moduleVersion,
-            supportedVersions: instrumentation.supportedVersions
-          })
-        })
-
-      return moduleExports
+    return moduleExports
   }
 
   /**
